80a563c285e231b54987af350007cc96cbd1e8cb
Jack Pustinger <jpusting@sacramento.cs.colostate.edu>
2025-12-03 12:00:10 -0700

    Implemented a few basic methods
    Implemented the following methods. init to intialize the 2D array based on total classroom and large classroom constraints. fillLargeClasses to fill the newly vreated array with large classes in the earliest possible time slots. primaryGoal calculates the total timeslots used, which is our job to minimize as the primary goal of our model. secondaryGoal calculates the number of wasted classes, which is our secondary goal to minimize. I put in a method stub for beginIteration, as I have an idea for how this should work but didn't have time to start on before class. My idea is that the method starts with the initial array and we only put small classes in small classrooms, and then we calculate the primary and secondary goals of the completed array and store them. We then decrease the number of small classes in small classrooms by 1 and put that 1 class in a big classroom, calculate the goals, and if its better than the first pass through, store it. We continue this for all possible configurations of small classrooms and store the best configuration, like finding the maximum of a number set.

--- Main.py
+++ Main.py
@@ -1,12 +1,65 @@
 import sys
+import math
 
 def main():
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         sys.exit(1)
-    smallClasses = int(sys.argv[0])
-    largeClasses = int(sys.argv[1])
-    smallClassrooms = int(sys.argv[2])
-    largeClassrooms = int(sys.argv[3])
+    Ns = int(sys.argv[1])
+    Nl = int(sys.argv[2])
+    S = int(sys.argv[3])
+    L = int(sys.argv[4])
+    myArray = init(Ns, Nl, S, L)
+    fillLargeClasses(myArray, Nl, L)
+    print(myArray)
+    print(primaryGoal(myArray))
+    print(secondaryGoal(myArray, Nl))
+    myArray[0][0] += 1
+    print(myArray)
+    print(primaryGoal(myArray))
+    print(secondaryGoal(myArray, Nl))
+    myArray[len(myArray) + 1 - primaryGoal(myArray)][0] += 1
+    print(myArray)
+    print(primaryGoal(myArray))
+    print(secondaryGoal(myArray, Nl))
+    #beginIteration(myArray, Ns, Nl, S, L)
+    
+#creates initial 2D array based on the greater of total classroom constraints and large classroom constraints
+def init(Ns, Nl, S, L):
+    constr1 = math.ceil((Ns + Nl) / (S + L))
+    constr2 = math.ceil(Nl / L)
+    rows = constr1 if constr1 > constr2 else constr2
+    columns = 2
+    returnArr = []
+    for i in range(rows):
+        row = [0] * columns
+        returnArr.append(row)
+    return returnArr
+
+#optimal solution always has large classes filled first, so this method is simple
+def fillLargeClasses(myArray, Nl, L):
+    for i in range(len(myArray)):
+        while(myArray[i][0] < L and Nl > 0):
+            myArray[i][0] += 1
+            Nl -= 1
+
+#finds number of time slots used
+def primaryGoal(myArray):
+    Zt = 0
+    for i in range(len(myArray)):
+        if(myArray[i][0] > 0 or myArray[i][0]):
+            Zt += 1
+    return Zt
+
+#finds number of wasted classes by finding total classes booked in large classrooms and subtracting number of large classes
+def secondaryGoal(myArray, Nl):
+    SLt = 0
+    for i in range(len(myArray)):
+        SLt += myArray[i][0]
+    return SLt - Nl
+
+#have idea but not sure if it is the most efficient, can discuss in class
+def beginIteration(myArray, Ns, Nl, S, L):
+    return
 
 if __name__ == "__main__":
     main()
