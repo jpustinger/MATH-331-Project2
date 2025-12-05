a60dcb284fab6ca2d116c784758bcf3ef61bfdcc
Jack Pustinger <jpusting@sacramento.cs.colostate.edu>
2025-12-05 12:55:56 -0700

    Begin iteration in progress
    Working on the iteration part, needs more time

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import copy
 
 def main():
     if len(sys.argv) != 5:
@@ -13,19 +14,11 @@ def main():
     print(myArray)
     print(primaryGoal(myArray))
     print(secondaryGoal(myArray, Nl))
-    myArray[0][0] += 1
-    print(myArray)
-    print(primaryGoal(myArray))
-    print(secondaryGoal(myArray, Nl))
-    myArray[len(myArray) + 1 - primaryGoal(myArray)][0] += 1
-    print(myArray)
-    print(primaryGoal(myArray))
-    print(secondaryGoal(myArray, Nl))
-    #beginIteration(myArray, Ns, Nl, S, L)
+    beginIteration(myArray, Ns, Nl, S, L)
     
 #creates initial 2D array based on the greater of total classroom constraints and large classroom constraints
 def init(Ns, Nl, S, L):
-    constr1 = math.ceil((Ns + Nl) / (S + L))
+    constr1 = math.ceil(Ns / S)
     constr2 = math.ceil(Nl / L)
     rows = constr1 if constr1 > constr2 else constr2
     columns = 2
@@ -58,8 +51,40 @@ def secondaryGoal(myArray, Nl):
     return SLt - Nl
 
 #have idea but not sure if it is the most efficient, can discuss in class
-def beginIteration(myArray, Ns, Nl, S, L):
+def beginIteration(startArray, Ns, Nl, S, L):
+    currArr = copy.deepcopy(startArray)
+    fillSmallClasses(currArr, S, L, Ns, 0)
+    bestArr = copy.deepcopy(currArr)
+    bestPrimary = primaryGoal(bestArr)
+    bestSecondary = secondaryGoal(bestArr, Nl)
+    print("Arr set to bestArr")      
+    for i in range(Ns):
+        #print(startArray)
+        diff = i + 1
+        currArr = copy.deepcopy(startArray)
+        if(Ns - diff > S * len(startArray) or diff + Nl > L * len(startArray)):
+            print("Input of SinS = " + str(Ns - diff) + " and SinL = " + str(diff) + " will not be optimal")
+            continue
+        fillSmallClasses(currArr, S, L, Ns - diff, diff)
+        if (bestPrimary(currArr) < bestPrimary(bestArr)):
+            print("New best found")
+            bestArr = copy.deepcopy(currArr)
+            bestPrimary = primaryGoal(bestArr)
+            bestSecondary = secondaryGoal(bestArr, Nl)
     return
+
+def fillSmallClasses(currArr, S, L, SinS, SinL):
+    print("Filled Array w/ SinS = " + str(SinS) + " and SinL = " + str(SinL) + ":")
+    for i in range(len(currArr)):
+        if SinS == 0 and SinL == 0:
+            break
+        while currArr[i][0] < L and SinL > 0:
+            currArr[i][0] += 1
+            SinL -= 1
+        while currArr[i][1] < S and SinS > 0:
+            currArr[i][1] += 1
+            SinS -= 1
+    print(currArr)
 
 if __name__ == "__main__":
     main()
